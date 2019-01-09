bfd7754109469b8ec0b9a42eff9983f182ad9123
brewchetta <chett.tiller@gmail.com>
2019-01-09 17:17:13 -0500

    Added login field

--- src/user/UserSignUp.js
+++ src/user/UserSignUp.js
@@ -5,7 +5,9 @@ export default class UserSignUp extends React.Component {
 
   state = {
     username: '',
-    password: ''
+    password: '',
+    login_username: '',
+    login_password: ''
   }
 
   handleChange = (event) => {
@@ -14,15 +16,19 @@ export default class UserSignUp extends React.Component {
 
   handleSubmit = (event) => {
     event.preventDefault()
-    fetchCreateUser(this.state)
+    if (event.target.name === 'signup') {
+      fetchCreateUser(this.state)
       .then(user => console.log(user))
+    } else if (event.target.name === 'login') {
+      console.log('logging in')
+    }
   }
 
   render() {
     return(
       <div>
         <p>Sign Up!</p>
-        <form onSubmit={this.handleSubmit}>
+        <form name='signup' onSubmit={this.handleSubmit}>
           <label name='username'>Name</label>
           <input type='text' name='username' value={this.state.username} onChange={this.handleChange}/>
 
@@ -30,6 +36,21 @@ export default class UserSignUp extends React.Component {
 
           <label name='password'>Password</label>
           <input type='text' name='password' value={this.state.password} onChange={this.handleChange}/>
+
+          <br/>
+
+          <input type='submit' />
+        </form>
+
+        <p>Log In!</p>
+        <form name='login' onSubmit={this.handleSubmit}>
+          <label name='login_username'>Name</label>
+          <input type='text' name='login_username' value={this.state.login_username} onChange={this.handleChange}/>
+
+          <br/>
+
+          <label name='login_password'>Password</label>
+          <input type='text' name='login_password' value={this.state.login_password} onChange={this.handleChange}/>
 
           <br/>
 
