a2ab4b6ceda5173429a648e379cfb5fdac95f798
brewchetta <chett.tiller@gmail.com>
2019-01-14 10:39:40 -0500

    Fixed a few issues with card names not aligning with backend names

--- src/card/CardIllustration.js
+++ src/card/CardIllustration.js
@@ -2,13 +2,11 @@ import React from 'react'
 import * as Animations from '../Animations'
 
 const CardIllustration = (props) => {
+  console.log(props.card)
 
   const card = props.card
   const ComponentName = Animations[card.card_name]
 
-  console.log(Animations[card.card_name])
-
-  // Possibly a large switch statement here or imported from nearby to determine the card's illustration
 
   return (
     <div className='card-illustration'>
