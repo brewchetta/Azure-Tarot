a546fd656cca4ecd08a05003923651c345f1d1d3
brewchetta <you@example.com>
2019-01-19 17:18:12 -0500

    Added a few assets and did some work on the fool

--- src/Animations/0_Fool.js
+++ src/Animations/0_Fool.js
@@ -1,9 +1,14 @@
 import React from 'react'
-// import asset from '../Assets/0_Fool.png'
+import seed from '../Assets/0_Fool_Seed.png'
 
-export const Fool = props => {
+export const Fool = () => {
+
   return (
-    <div className='card-animation'>
+    <div className='card-animation' style={{ background: 'white' }}>
+
+      <img alt='' src={seed} className='fool-sway'
+      style={{ position: 'absolute', top: '0', left: '0', width: '100%'}}
+      />
 
     </div>
   )
