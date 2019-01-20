3382fbc97a5e734197ba14a53a396d88df305495
brewchetta <you@example.com>
2019-01-20 12:04:20 -0500

    Did some work enhancing a few animations

--- src/Animations/1_Magician.js
+++ src/Animations/1_Magician.js
@@ -1,7 +1,10 @@
-// Hand holds the rotating animation I built a while ago, along with the four symbols
 import React from 'react'
 import hand from '../Assets/1_Magician_Hand.png'
 import fingers from '../Assets/1_Magician_Fingers.png'
+import cup from '../Assets/Symbol_Chalice.png'
+import wand from '../Assets/Symbol_Broom.png'
+import pentacle from '../Assets/Symbol_Pentacle.png'
+import sword from '../Assets/Symbol_Sword.png'
 
 export const Magician = props => {
   return (
@@ -9,6 +12,20 @@ export const Magician = props => {
       <img alt='' src={hand}
       style={{ height: '100%', width: '100%' }} />
 
+      {/* The Four Suits */}
+      <img alt='' src={cup} className='magician-symbols'
+      style={{ left: '50%', top: '47%' }} />
+
+      <img alt='' src={wand} className='magician-symbols'
+      style={{ animationDelay: '2.5s', left: '51%', top: '45%', height: '18%', transform: 'rotate(180deg)' }} />
+
+      <img alt='' src={pentacle} className='magician-symbols'
+      style={{ animationDelay: '5s', left: '47%', top: '45%' }} />
+
+      <img alt='' src={sword} className='magician-symbols'
+      style={{ animationDelay: '7.5s', left: '54%', top: '43%', height: '19%' }} />
+
+      {/* Rotating Flower */}
       <div className='magician-rotator-container'>
         <div className='magician-rotator' style={{ animation: 'm-rotate 16s infinite linear'  }}>
           <div className='magician-rotator' style={{ animation: 'm-rotate 16s infinite linear'  }}>
@@ -26,6 +43,7 @@ export const Magician = props => {
         </div>
       </div>
 
+      {/* Fingers go last so they overlay other animations */}
       <img alt='' src={fingers}
       style={{ height: '100%', width: '100%', position: 'absolute', left: '0', top: '0', zIndex: '1' }} />
 
--- src/Animations/8_Strength.js
+++ src/Animations/8_Strength.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import cat from '../Assets/8_Strength_Cat.png'
 import lion from '../Assets/8_Strength_Lion.png'
+import birch from '../Assets/8_Strength_Birch.png'
 
 export const Strength = props => {
   return (
     <div className='card-animation' style={{ backgroundColor: '#4d0f00' }}>
+
+      <img alt='' src={birch} className='strength-birch' />
 
       <img alt='' src={cat} className='strength-cat' />
 
