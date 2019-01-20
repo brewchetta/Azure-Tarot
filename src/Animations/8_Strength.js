f4e7820fe447c2be879ec7ebbeec6d6b3b64e1da
brewchetta <you@example.com>
2019-01-19 22:15:49 -0500

    Added strength animation, minor tweaks to high priestess

--- src/Animations/8_Strength.js
+++ src/Animations/8_Strength.js
@@ -1,9 +1,17 @@
 import React from 'react'
-// import asset from '../Assets/8_Strength.png'
+import cat from '../Assets/8_Strength_Cat.png'
+import lion from '../Assets/8_Strength_Lion.png'
 
 export const Strength = props => {
   return (
-    <div className='card-animation'>
+    <div className='card-animation' style={{ backgroundColor: '#4d0f00' }}>
+
+      <img alt='' src={cat} className='strength-cat' />
+
+      <img alt='' src={lion} className='strength-lion' />
+
+      <div className='strength-mirror'>
+      </div>
 
     </div>
   )
