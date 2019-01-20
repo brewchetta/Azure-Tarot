7cddbb912b683806e345fc866b671822e6b51286
brewchetta <you@example.com>
2019-01-19 20:46:09 -0500

    More animations like the emperor

--- src/Animations/4_Emperor.js
+++ src/Animations/4_Emperor.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import watch from '../Assets/4_Emperor_Watch.png'
+import starmap from '../Assets/4_Emperor_Starmap.png'
 
 export const Emperor = props => {
   return (
     <div className='card-animation'>
       <img alt='' src={watch}
-      className='card-fit-height'
-      style={{ position: 'relative', top: '-45%', left: '-5%' }} />
+      style={{ position: 'absolute', top: '45%', left: '-2%', width: '100%', zIndex: '1' }} />
+
+      <img alt='' src={starmap} className='emperor-starmap' />
+
     </div>
   )
 }
