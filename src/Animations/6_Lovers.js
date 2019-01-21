b4804e630a942d7589312af338adbd1d02db73ac
brewchetta <you@example.com>
2019-01-21 10:54:53 -0500

    Revamped the lovers, they're now adorable barn owls

--- src/Animations/6_Lovers.js
+++ src/Animations/6_Lovers.js
@@ -1,15 +1,42 @@
 import React from 'react'
-import mothOne from '../Assets/6_Lovers_Moth_One.png'
-import mothTwo from '../Assets/6_Lovers_Moth_Two.png'
+import owls from '../Assets/6_Lovers_Owls.png'
+import forest from '../Assets/6_Lovers_Forest.png'
 
 export const Lovers = props => {
   return (
     <div className='card-animation'>
-      <img alt='' src={mothOne}
-      style={{ height: '50%', transform: 'rotate(190deg)' }} />
+      <img alt='' src={forest}
+      style={{ left: '0', position: 'absolute', top: '0', width: '100%' }} />
 
-      <img alt='' src={mothTwo}
-      style={{ height: '50%', position: 'relative', transform: 'rotate(-15deg)' }} />
+      <div className='lovers-snow' style={{ left: `${Math.random() * 100}%`, animationDelay: `${Math.random() * 8 - 5}s`, transform: `rotate(${Math.random() * 360}deg)` }}></div>
+
+      <div className='lovers-snow' style={{ left: `${Math.random() * 100}%`, animationDelay: `${Math.random() * 8 - 5}s`, transform: `rotate(${Math.random() * 360}deg)` }}></div>
+
+      <div className='lovers-snow' style={{ left: `${Math.random() * 100}%`, animationDelay: `${Math.random() * 8 - 5}s`, transform: `rotate(${Math.random() * 360}deg)` }}></div>
+
+      <div className='lovers-snow' style={{ left: `${Math.random() * 100}%`, animationDelay: `${Math.random() * 8 - 5}s`, transform: `rotate(${Math.random() * 360}deg)` }}></div>
+
+      <div className='lovers-snow' style={{ left: `${Math.random() * 100}%`, animationDelay: `${Math.random() * 8 - 5}s`, transform: `rotate(${Math.random() * 360}deg)` }}></div>
+
+      <div className='lovers-snow' style={{ left: `${Math.random() * 100}%`, animationDelay: `${Math.random() * 8 - 5}s`, transform: `rotate(${Math.random() * 360}deg)` }}></div>
+
+      <div className='lovers-snow' style={{ left: `${Math.random() * 100}%`, animationDelay: `${Math.random() * 8 - 5}s`, transform: `rotate(${Math.random() * 360}deg)` }}></div>
+
+      <div className='lovers-snow' style={{ left: `${Math.random() * 100}%`, animationDelay: `${Math.random() * 8 - 8}s`, transform: `rotate(${Math.random() * 360}deg)` }}></div>
+
+      <div className='lovers-snow' style={{ left: `${Math.random() * 100}%`, animationDelay: `${Math.random() * 8 - 5}s`, transform: `rotate(${Math.random() * 360}deg)` }}></div>
+
+      <div className='lovers-snow' style={{ left: `${Math.random() * 100}%`, animationDelay: `${Math.random() * 8 - 5}s`, transform: `rotate(${Math.random() * 360}deg)` }}></div>
+
+      <div className='lovers-snow' style={{ left: `${Math.random() * 100}%`, animationDelay: `${Math.random() * 8 - 5}s`, transform: `rotate(${Math.random() * 360}deg)` }}></div>
+
+      <div className='lovers-snow' style={{ left: `${Math.random() * 100}%`, animationDelay: `${Math.random() * 8 - 5}s`, transform: `rotate(${Math.random() * 360}deg)` }}></div>
+
+      <div className='lovers-snow' style={{ left: `${Math.random() * 100}%`, animationDelay: `${Math.random() * 8 - 5}s`, transform: `rotate(${Math.random() * 360}deg)` }}></div>
+
+      <img alt='' src={owls}
+      style={{ left: '0', position: 'absolute', bottom: '0', width: '100%' }} />
+
     </div>
   )
 }
