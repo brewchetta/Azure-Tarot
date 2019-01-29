2111e53561a16300984384c4148fcba64d28ddb8
brewchetta <chett.tiller@gmail.com>
2019-01-29 12:55:39 -0500

    Updates

---
+++ precache-manifest.e1f409aa50bce79c00a8fc23ae3cc8a2.js
@@ -0,0 +1,242 @@
+self.__precacheManifest = [
+  {
+    "revision": "91b9bbfe35764060a72f",
+    "url": "/Azure-Tarot/static/css/main.f5a03bce.chunk.css"
+  },
+  {
+    "revision": "91b9bbfe35764060a72f",
+    "url": "/Azure-Tarot/static/js/main.91b9bbfe.chunk.js"
+  },
+  {
+    "revision": "f96a78d7b34cf2e49d1c",
+    "url": "/Azure-Tarot/static/js/1.f96a78d7.chunk.js"
+  },
+  {
+    "revision": "386d4d89d4e185995b30",
+    "url": "/Azure-Tarot/static/js/runtime~main.386d4d89.js"
+  },
+  {
+    "revision": "35fb7f542e48e30d65352b78bda80e81",
+    "url": "/Azure-Tarot/static/media/19_Sun_Seeds.35fb7f54.png"
+  },
+  {
+    "revision": "506e976c6ac496317fac67d94308830a",
+    "url": "/Azure-Tarot/static/media/18_Moon_Firefly.506e976c.png"
+  },
+  {
+    "revision": "ab7bd0a8acb0c692eabaebb71a32e32d",
+    "url": "/Azure-Tarot/static/media/2_High_Priestess_Inkblot.ab7bd0a8.png"
+  },
+  {
+    "revision": "842db569d6eaa6e488b080e54abe4409",
+    "url": "/Azure-Tarot/static/media/5_Hierophant_Feather.842db569.png"
+  },
+  {
+    "revision": "782260049bac5e24d78decab3a22c6c8",
+    "url": "/Azure-Tarot/static/media/16_Tower_Cloud.78226004.png"
+  },
+  {
+    "revision": "d6903ae63617266e0435213aaab09340",
+    "url": "/Azure-Tarot/static/media/Corner_Leaf.d6903ae6.png"
+  },
+  {
+    "revision": "084efe82dd748c0321f5cc49bbce2cdb",
+    "url": "/Azure-Tarot/static/media/7_Chariot_Wheel.084efe82.png"
+  },
+  {
+    "revision": "f346be42b511a64a50143ed86130aae2",
+    "url": "/Azure-Tarot/static/media/Loading_Sun.f346be42.png"
+  },
+  {
+    "revision": "c3872c99582d4f657078c7eed14ccf8e",
+    "url": "/Azure-Tarot/static/media/Loading_Moon.c3872c99.png"
+  },
+  {
+    "revision": "55dc940b08be00f54c6d5c2c5f5b8b65",
+    "url": "/Azure-Tarot/static/media/0_Fool_Seed.55dc940b.png"
+  },
+  {
+    "revision": "97f18bd2464ec55657cb9ac5c6c9f447",
+    "url": "/Azure-Tarot/static/media/1_Magician_Hand.97f18bd2.png"
+  },
+  {
+    "revision": "9ac39cfb4c0a0aa0376a6b06902b448c",
+    "url": "/Azure-Tarot/static/media/1_Magician_Fingers.9ac39cfb.png"
+  },
+  {
+    "revision": "78c1be09265e3f5825d5a316f845a4dc",
+    "url": "/Azure-Tarot/static/media/2_High_Priestess_Cat.78c1be09.png"
+  },
+  {
+    "revision": "376ec4a8c61f0af5eb70ba97371c2db1",
+    "url": "/Azure-Tarot/static/media/3_Empress_Bears.376ec4a8.png"
+  },
+  {
+    "revision": "8d6904e37c5c6c7d5ad9f6e41d2b5e12",
+    "url": "/Azure-Tarot/static/media/3_Empress_Background.8d6904e3.png"
+  },
+  {
+    "revision": "4614bad02a520fd886755b8a2d31b900",
+    "url": "/Azure-Tarot/static/media/4_Emperor_Watch.4614bad0.png"
+  },
+  {
+    "revision": "9a6a36fe6cc85293d15ad1429a43066f",
+    "url": "/Azure-Tarot/static/media/4_Emperor_Starmap.9a6a36fe.png"
+  },
+  {
+    "revision": "f50a3024ef9bf5ce2eb343725587916e",
+    "url": "/Azure-Tarot/static/media/5_Hierophant_Raven.f50a3024.png"
+  },
+  {
+    "revision": "a664e0447f1b2de06f3fcff23f2ff0ed",
+    "url": "/Azure-Tarot/static/media/5_Hierophant_Raven_Head.a664e044.png"
+  },
+  {
+    "revision": "fc9a034e90e1109100c8682e76a81804",
+    "url": "/Azure-Tarot/static/media/5_Hierophant_Books.fc9a034e.png"
+  },
+  {
+    "revision": "59d5c97f71ecc6e770efe44a52a80c45",
+    "url": "/Azure-Tarot/static/media/6_Lovers_Owls.59d5c97f.png"
+  },
+  {
+    "revision": "dd7165cee4d65062e434fd61ccae91a1",
+    "url": "/Azure-Tarot/static/media/6_Lovers_Forest.dd7165ce.png"
+  },
+  {
+    "revision": "7f009b906d14d550b8afd8b26d5b33dc",
+    "url": "/Azure-Tarot/static/media/8_Strength_Cat.7f009b90.png"
+  },
+  {
+    "revision": "bf125fd9db5b632f7507d6c53a216949",
+    "url": "/Azure-Tarot/static/media/8_Strength_Lion.bf125fd9.png"
+  },
+  {
+    "revision": "56e204138d9028568ab02ed5cfe8b57d",
+    "url": "/Azure-Tarot/static/media/8_Strength_Birch.56e20413.png"
+  },
+  {
+    "revision": "81cccc2042d7548c53d5eb5d1e064b78",
+    "url": "/Azure-Tarot/static/media/9_Hermit_Snail_Background.81cccc20.png"
+  },
+  {
+    "revision": "6b2a16536e691277527bbb8b12a3cfaf",
+    "url": "/Azure-Tarot/static/media/9_Hermit_Snail_Foreground.6b2a1653.png"
+  },
+  {
+    "revision": "a6110d7916b44e98e48f4238a276adc6",
+    "url": "/Azure-Tarot/static/media/10_Wheel_Ink.a6110d79.png"
+  },
+  {
+    "revision": "2b9fb7bc27f60b9f230eac61373bc9fa",
+    "url": "/Azure-Tarot/static/media/10_Wheel_Knot.2b9fb7bc.png"
+  },
+  {
+    "revision": "81ddfd94d40c92bcca046fcbc39ae9dc",
+    "url": "/Azure-Tarot/static/media/10_Wheel_Planet.81ddfd94.png"
+  },
+  {
+    "revision": "ee52707158899b0db709d7ce6625d6e5",
+    "url": "/Azure-Tarot/static/media/10_Wheel_Snake.ee527071.png"
+  },
+  {
+    "revision": "73219b13da8246ce986f6b02222a5302",
+    "url": "/Azure-Tarot/static/media/11_Justice_Sword.73219b13.png"
+  },
+  {
+    "revision": "a0697d2efb568a75fa4cb2b871cda2f9",
+    "url": "/Azure-Tarot/static/media/11_Justice_Winter.a0697d2e.png"
+  },
+  {
+    "revision": "b956b4cbe6be274c63b589be8b5e9548",
+    "url": "/Azure-Tarot/static/media/11_Justice_Spring.b956b4cb.png"
+  },
+  {
+    "revision": "89304311f9aa62542443dcc0dee678c8",
+    "url": "/Azure-Tarot/static/media/12_HangedMan_Chrysalis.89304311.png"
+  },
+  {
+    "revision": "ba4ea069235ef6ff113b9fb90d29595c",
+    "url": "/Azure-Tarot/static/media/12_HangedMan_Fern.ba4ea069.png"
+  },
+  {
+    "revision": "c0f0d864fbbcec9b16a70623c0429ed8",
+    "url": "/Azure-Tarot/static/media/13_Death_Cat.c0f0d864.png"
+  },
+  {
+    "revision": "b02d851899ec92ee41828b9c5243d8be",
+    "url": "/Azure-Tarot/static/media/13_Death_Tree.b02d8518.png"
+  },
+  {
+    "revision": "ee275fe4b75a787d9e009c7f39a195da",
+    "url": "/Azure-Tarot/static/media/14_Temperance_Waterfall.ee275fe4.png"
+  },
+  {
+    "revision": "2e8099f45d7c24069d3eea1f6d943fc0",
+    "url": "/Azure-Tarot/static/media/15_Devil_Eye_Background.2e8099f4.png"
+  },
+  {
+    "revision": "50176d7ae4f6c981b27bc26631a64c9b",
+    "url": "/Azure-Tarot/static/media/15_Devil_Eye_Foreground.50176d7a.png"
+  },
+  {
+    "revision": "ea5e59ea35f0c129eaaffbaa94fa684d",
+    "url": "/Azure-Tarot/static/media/15_Devil_Pupil.ea5e59ea.png"
+  },
+  {
+    "revision": "fb3eb02c4cdf429a60a4611ef041676d",
+    "url": "/Azure-Tarot/static/media/16_Tower_Baobab.fb3eb02c.png"
+  },
+  {
+    "revision": "b9b28268c967778e31c1ba33aec3ca99",
+    "url": "/Azure-Tarot/static/media/17_Star_Lighthouse.b9b28268.png"
+  },
+  {
+    "revision": "5916629598921717aae17efeb2a4c87e",
+    "url": "/Azure-Tarot/static/media/17_Star_Northstars.59166295.png"
+  },
+  {
+    "revision": "fb65cd11dbb12b41bc336c194f9ef1a8",
+    "url": "/Azure-Tarot/static/media/18_Moon_Fullmoon.fb65cd11.png"
+  },
+  {
+    "revision": "e332eb59ba06f18090654f4d1018f337",
+    "url": "/Azure-Tarot/static/media/18_Moon_Grass.e332eb59.png"
+  },
+  {
+    "revision": "67a9f3e52ca3d178d9426a3c47fd6757",
+    "url": "/Azure-Tarot/static/media/19_Sun_Dandelion.67a9f3e5.png"
+  },
+  {
+    "revision": "efb84bf8024312af20b1d6f30065e863",
+    "url": "/Azure-Tarot/static/media/20_Judgement_Hawk.efb84bf8.png"
+  },
+  {
+    "revision": "07027157fd37f434b81255422f51066d",
+    "url": "/Azure-Tarot/static/media/20_Judgement_Sun.07027157.png"
+  },
+  {
+    "revision": "d5f75c8cd7c175f28f82c74a41f23905",
+    "url": "/Azure-Tarot/static/media/21_World_Forest.d5f75c8c.png"
+  },
+  {
+    "revision": "6980515e90da35d19e49b19dc8dec705",
+    "url": "/Azure-Tarot/static/media/21_World_Mountains.6980515e.png"
+  },
+  {
+    "revision": "e06bd86bb5ee4cb408761f00018c12ff",
+    "url": "/Azure-Tarot/static/media/21_World_Town.e06bd86b.png"
+  },
+  {
+    "revision": "abdb98cae5b2cc719b7b58e1188791a1",
+    "url": "/Azure-Tarot/static/media/21_World_Clouds.abdb98ca.png"
+  },
+  {
+    "revision": "9e53c6c8e4a26c98d03220280e87f9aa",
+    "url": "/Azure-Tarot/static/media/Card_Back.9e53c6c8.png"
+  },
+  {
+    "revision": "84fc4a1656d74e8f8bce372d2980fbe6",
+    "url": "/Azure-Tarot/index.html"
+  }
+];
