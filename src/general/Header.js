227237a1fa36b3a87293111b633dc06e9d40a5c6
brewchetta <chett.tiller@gmail.com>
2019-03-05 14:17:10 -0500

    More responsiveness

--- src/general/Header.js
+++ src/general/Header.js
@@ -7,11 +7,11 @@ export default function Header(props) {
 
   return (
     <header>
-      <img alt='' src={symbols} className='header-symbols-left' />
-      <img alt='' src={symbols} className='header-symbols-right' />
+      <img alt='' src={symbols} className='header-symbols-left large-screen-only' />
+      <img alt='' src={symbols} className='header-symbols-right large-screen-only' />
       <img alt='' src={corner} className='header-corner' style={{ right: '0' }} />
       <img alt='' src={corner} className='header-corner' style={{ left: '0', transform: 'rotateY(180deg)' }} />
-      <h1>Azure Tarot</h1>
+      <h1 className='small-screen-remove'>Azure Tarot</h1>
     </header>
   )
 }
