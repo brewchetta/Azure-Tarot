8c01c3375b5e7859aad22410e375846583496146
brewchetta <you@example.com>
2019-03-18 18:35:56 -0400

    Pentacles now have stand in assets

---
+++ src/Animations/Pentacles/8_Pentacles.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import pentacle from '../../Assets/Symbol_Pentacle.png'
+
+export const EightOfPentacles = () => {
+
+  return (
+    <div className='card-animation' style={{ background: 'white' }}>
+
+      <img alt='' src={pentacle}
+      style={{ position: 'absolute', top: '0', left: '0', height: '100%'}}
+      />
+
+    </div>
+  )
+}
