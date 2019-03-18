021d19805e998deb837dd39ba48c77e82d4748d7
brewchetta <you@example.com>
2019-03-18 18:40:42 -0400

    Stand in animations for swords

---
+++ src/Animations/Swords/9_Swords.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import sword from '../../Assets/Symbol_Sword.png'
+
+export const NineOfSwords = () => {
+
+  return (
+    <div className='card-animation' style={{ background: 'white' }}>
+
+      <img alt='' src={sword}
+      style={{ position: 'absolute', top: '0', left: '0', height: '100%'}}
+      />
+
+    </div>
+  )
+}
