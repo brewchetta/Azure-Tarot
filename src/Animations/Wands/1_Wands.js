af0b2b5438c04cb9a7f51287462819049f93c552
brewchetta <you@example.com>
2019-03-18 18:44:42 -0400

    Stand in assets for wands

---
+++ src/Animations/Wands/1_Wands.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import broom from '../../Assets/Symbol_Broom.png'
+
+export const AceOfWands = () => {
+
+  return (
+    <div className='card-animation' style={{ background: 'white' }}>
+
+      <img alt='' src={broom}
+      style={{ position: 'absolute', top: '0', left: '0', height: '100%'}}
+      />
+
+    </div>
+  )
+}
---
+++ src/Animations/Wands/9_Wands.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import broom from '../../Assets/Symbol_Broom.png'
+
+export const NineOfWands = () => {
+
+  return (
+    <div className='card-animation' style={{ background: 'white' }}>
+
+      <img alt='' src={broom}
+      style={{ position: 'absolute', top: '0', left: '0', height: '100%'}}
+      />
+
+    </div>
+  )
+}
