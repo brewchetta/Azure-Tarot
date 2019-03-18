48780731fbe1a5b32e187e5e61bc4c9343d7c12e
brewchetta <you@example.com>
2019-03-18 17:41:30 -0400

    Built out template for cups animations

---
+++ src/Animations/Cups/8_Cups.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import seed from '../../Assets/0_Fool_Seed.png'
+
+export const EightOfCups = () => {
+
+  return (
+    <div className='card-animation' style={{ background: 'white' }}>
+
+      <img alt='' src={seed} className='fool-sway'
+      style={{ position: 'absolute', top: '0', left: '0', width: '100%'}}
+      />
+
+    </div>
+  )
+}
